3aeaf9f6e8007b0f1547c524ab8f49caa8adfc96
yangyujie-jack <yangyj21@mails.tsinghua.edu.cn>
2025-04-21 12:32:43 +0800

    update smooth logic

--- examples/get_result.py
+++ examples/get_result.py
@@ -13,8 +13,10 @@ LOGTAGS = {
     'return': 'Metrics/EpRet',
 }
 
+
 def epoch_to_step(epoch):
     return (epoch + 1) * 4000
+
 
 def extract_training_data(
     envs: Sequence[str],
@@ -29,27 +31,47 @@ def extract_training_data(
                 step = epoch_to_step(log['Train/Epoch'])
                 seed = str(int(log_dir_name.split('-')[1]))
                 for tag in tags:
-                    df = pd.DataFrame({
-                        'step': step,
-                        'value': log[LOGTAGS[tag]],
-                    })
+                    df = pd.DataFrame(
+                        {
+                            'step': step,
+                            'value': smooth(log[LOGTAGS[tag]]),
+                        }
+                    )
                     result_dir = os.path.join(RESULT_PATH, env, tag)
                     os.makedirs(result_dir, exist_ok=True)
                     result_file_name = '_'.join([alg, seed]) + '.csv'
                     result_file = os.path.join(result_dir, result_file_name)
                     df.to_csv(result_file, index=False)
 
+
+def smooth(x: pd.Series):
+    # sliding window with length 2
+    return pd.concat((x[:1], (x[:-1] + x[1:]) / 2))
+
+
 envs = [
     'SafetyPointGoal1-v0',
     'SafetyPointPush1-v0',
+    'SafetyPointCircle1-v0',
     'SafetyCarGoal1-v0',
     'SafetyCarPush1-v0',
+    'SafetyCarCircle1-v0',
+    'SafetyAntVelocity-v1',
+    'SafetyHumanoidVelocity-v1',
+    'SafetyPointButton1-v0',
+    'SafetyCarButton1-v0',
+    'SafetyHalfCheetahVelocity-v1',
+    'SafetyHopperVelocity-v1',
+    'SafetyWalker2dVelocity-v1',
+    'SafetySwimmerVelocity-v1',
 ]
 
 algs = [
     'CPO',
     'RCPO',
     'PPOLag',
+    'FOCOPS',
+    'CUP',
 ]
 
 tags = [
